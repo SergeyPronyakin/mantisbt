122aff501e20157d4427f64ba57155b073867a38
SergeyPronyakin <spkobra2@gmail.com>
2023-05-29 23:17:35 +0300

    Connection failed

--- conftest.py
+++ conftest.py
@@ -4,9 +4,27 @@ from fixture.application import Application
 import pytest
 from fixture.db import DbFixture
 from model.user import User
+import ftputil
 
 fixture = None
 target = None
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            remote.remove("config_inc.php.bak")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.bak", "config_inc.php")
 
 
 def load_config(file):
@@ -18,15 +36,29 @@ def load_config(file):
     return target
 
 
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
-    web_config = load_config(request.config.getoption("--target"))
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=web_config["web"]["browser"], base_url=web_config["web"]["baseUrl"])
-        fixture.session.ensure_login(User(username=web_config["adminweb"]["user"],
-                                          password=web_config["adminweb"]["password"]))
+        fixture = Application(browser=config["web"]["browser"], base_url=config["web"]["baseUrl"])
+        fixture.session.ensure_login(User(username=config["adminweb"]["user"],
+                                          password=config["adminweb"]["password"]))
     return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
+
+    def fin():
+        restore_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
+
+    request.addfinalizer(fin)
 
 
 @pytest.fixture(scope="session")
